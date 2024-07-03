dfdf50377c84abe6b6877456e518222882ea05bc
icet-sharadha <icet.sharadha@gmail.com>
2024-07-03 11:48:46 +0530

    search

--- app.js
+++ app.js
@@ -13,9 +13,39 @@ fetch("https://restcountries.com/v3.1/all")
                     <td>${element.name.common}</td>
                     <td>${element.flag}</td>
                 </tr>`
-
-        console.log(element.name.common);
     });
 
     tblCountries.innerHTML=tblBody;
 })
+
+
+
+// https://restcountries.com/v3.1/name/{name}
+
+
+function serchCuntrie(){
+    let searchValue=document.getElementById("txtSearchValue").value;
+
+    let offitalName = document.getElementById("offitalName");
+   let name = document.getElementById("name")
+   let img =  document.getElementById("img")
+
+
+    console.log(searchValue);
+    fetch(`https://restcountries.com/v3.1/name/${searchValue}`)
+    .then(res=>res.json())
+    .then(data =>{
+
+        data.forEach(obj=>{
+            offitalName.innerText = obj.name.official;
+            name.innerText = obj.name.common;
+     
+            img.innerHTML=`<img src="${obj.flags.png}" alt="">`
+        })
+        console.log(data);
+
+
+
+    
+    })
+}
